1454a40a377d31b3df987ccc14878cb009227781
Roberto B <roberto.butti@gmail.com>
2020-06-17 07:51:13 +0200

    Updating tests

--- tests/ConsistencyTest.php
+++ tests/ConsistencyTest.php
@@ -30,7 +30,7 @@ class ConsistencyTest extends TestCase
         $l = new LaraLens();
         $a = $l->getRuntimeConfigs();
         $this->assertIsArray($a->toArray());
-        $this->assertEquals(4, count($a->toArray()));
+        $this->assertEquals(6, count($a->toArray()));
     }
     /** @test */
     public function test_database_array()
@@ -38,8 +38,8 @@ class ConsistencyTest extends TestCase
         $l = new LaraLens();
         $a = $l->getDatabase();
         $this->assertIsArray($a->toArray());
-        $this->assertGreaterThanOrEqual(5, count($a->toArray()));
-        $this->assertLessThanOrEqual(6, count($a->toArray()));
+        $this->assertGreaterThanOrEqual(8, count($a->toArray()));
+        $this->assertLessThanOrEqual(12, count($a->toArray()));
 
     }
 
