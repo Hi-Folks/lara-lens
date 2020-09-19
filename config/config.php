3645ba10b8502255f24dbc0ea021e473e7c21815
Roberto B <roberto.butti@gmail.com>
2020-09-19 20:51:44 +0200

    create config
    - prefix: url prefix for accessing to page
    -  middleware : middleware to use when accessing the controller (default web)
    - web-enabled : on. | off on. if you want to enable the page. if it is not "on" you will receive 401 as http status

--- config/config.php
+++ config/config.php
@@ -4,5 +4,7 @@
  * You can place your custom package configuration in here.
  */
 return [
-
+    'prefix' => 'laralens',
+    'middleware' => ['web'],
+    'web-enabled' => 'on' // 'on' to activate LaraLens Web view
 ];
