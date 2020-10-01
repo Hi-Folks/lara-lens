c93f3000399ab163937d9255a0fc0eba1af0aa70
Jeferson Oliveira <zuruckyt@gmail.com>
2020-10-01 13:23:33 -0300

    Update comment to match changes

--- config/config.php
+++ config/config.php
@@ -5,6 +5,6 @@
  */
 return [
     'prefix' => env('LARALENS_PREFIX', 'laralens'), // URL prefix (default=laralens)
-    'middleware' => explode(';', env('LARALENS_MIDDLEWARE', 'web')), // middleware (default=web) more separate with ,
+    'middleware' => explode(';', env('LARALENS_MIDDLEWARE', 'web')), // middleware (default=web) more separate with ;
     'web-enabled' => env('LARALENS_WEB_ENABLED', 'off') // Activate web view (default=off)
 ];
